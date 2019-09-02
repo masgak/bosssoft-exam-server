56c9ee1eff8248f0e80fad24eda10ef40d09979c
dev01 <dev01@email.com>
2019-09-02 20:20:46 +0800

    题目管理的查询操作

---
+++ boss-bes-user-permission-center/boss-bes-user-permission-dao/src/test/java/com/bosssoft/bes/userpermission/dao/SubjectDaoTest.java
@@ -0,0 +1,15 @@
+package com.bosssoft.bes.userpermission.dao;
+
+import com.bosssoft.bes.userpermission.pojo.dto.SubjectDTO;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class SubjectDaoTest {
+    @Autowired
+    SubjectDao subjectDao;
+    @Test
+    public void test(){
+    }
+}
---
+++ boss-bes-user-permission-center/boss-bes-user-permission-service/src/test/java/com/bosssoft/bes/userpermission/service/SubjectServiceTest.java
@@ -0,0 +1,14 @@
+package com.bosssoft.bes.userpermission.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SubjectServiceTest {
+    @Autowired
+    SubjectService subjectService;
+
+    @Test
+    public void test(){
+
+    }
+}
